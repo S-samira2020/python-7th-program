3772df3c48a8af8c387f57e6c52db6c3146e8a0f
Samira <54632872+S-samira2020@users.noreply.github.com>
2020-09-11 07:40:51 +0600

    Add files via upload

---
+++ program2.py
@@ -0,0 +1,25 @@
+# Touple
+
+'''dimension = (200, 50)
+print(dimension[0])
+print(dimension[1])
+print()
+
+# using for loop [ Touple ]
+
+for names in dimension:
+    print(names)
+print()
+'''
+
+dimensions = (200, 50)
+print('Original dimensions: ')
+for dimension in dimensions:
+    print(dimension)
+print()
+
+dimensions = (400, 100)
+print('modified dimensions: ')
+for dimension in dimensions:
+    print(dimension)
+print()
---
+++ program3.py
@@ -0,0 +1,18 @@
+# 4.13
+foods = ('drink', 'ice-cream', 'faluda', 'custard', 'sweets')
+print('original menu: ')
+for food  in foods:
+    print(food)
+print()
+
+food = ('drink', 'faluda', 'custard', 'sweets', 'pastries')
+print('one item modified menu:  ')
+for food in foods:
+    print(food)
+print()
+
+foods = ('drink', 'ice-cream', 'faluda', 'pastries', 'zera pani')
+print('modified menu: ')
+for food in foods:
+    print(food)
+print()
